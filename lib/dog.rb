33ff92919a02d230a546930b8129372084a9d078
Moose Davis <moosedavis2011@gmail.com>
2020-09-17 13:36:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,17 @@ class Dog
   def name 
     @name
   end 
-end
+
+  def breed=(breed)
+    @breed = breed
+  end 
+  
+  def breed
+    @breed 
+  end 
+end 
 
 fido = Dog.new
 fido.name
+
+snoopy = Dog.new 
